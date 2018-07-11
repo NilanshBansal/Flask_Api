7072e36f01d55990b0d371ece12dc5e03754add9
Nilansh Bansal <bansalnilansh@gmail.com>
2018-07-11 23:46:26 +0530

    serving html pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 app = Flask(__name__)
 
 stores = [
@@ -15,7 +15,7 @@ stores = [
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 @app.route('/store',methods=['POST'])
 def create_store():
